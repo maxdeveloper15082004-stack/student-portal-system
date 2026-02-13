af445be4e32cf17fb09bff8213467a7ea6aa653c
maxdeveloper15082004-stack <maxdeveloper15082004@gmail.comk>
2026-02-14 01:06:30 +0530

    delete unwanted database

--- student_portal/settings.py
+++ student_portal/settings.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+import dj_database_url
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -59,10 +60,10 @@ TEMPLATES = [
 WSGI_APPLICATION = "student_portal.wsgi.application"
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(
+        default="sqlite:///" + str(BASE_DIR / "db.sqlite3"),
+        conn_max_age=600,
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
