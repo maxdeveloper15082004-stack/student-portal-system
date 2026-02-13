dae587a21b9101a892bf816d9c84dc382a1fc811
maxdeveloper15082004-stack <maxdeveloper15082004@gmail.comk>
2026-02-13 16:10:38 +0530

    Clean requirements

--- student_portal/settings.py
+++ student_portal/settings.py
@@ -32,7 +32,10 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
 ]
+
 
 ROOT_URLCONF = "student_portal.urls"
 
@@ -85,7 +88,9 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
